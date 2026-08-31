eb47ac4873e43d355b137a331a610201ba05d297
alvarodaramendiz <alvarodaramendiz@unicesar.edu.co>
2026-08-31 14:26:51 -0500

    feat: GameZone project inicialization

---
+++ src/main/java/com/gamezone/taller/gamezoneunicesar/p3/TallerGameZoneUnicesarP3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.gamezone.taller.gamezoneunicesar.p3;
+
+/**
+ *
+ * @author ALVARO
+ */
+public class TallerGameZoneUnicesarP3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
