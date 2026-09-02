434d703a26469f49fa96cfedb5b12a694332eb2c
estefaniamarquez <estefaniamarquez@unicesar.edu.co>
2026-09-02 11:42:27 -0500

    feat: Development of abstract class 'Person'

--- src/main/java/model/Person.java
+++ src/main/java/model/Person.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author ALVARO
- */
 public class Person {
+    private String name;
+    private long iD;
+    private long contactNumber;
+
+    public Person() {
+    }
+
+    public Person(String name, long iD, long contactNumber) {
+        this.name = name;
+        this.iD = iD;
+        this.contactNumber = contactNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getiD() {
+        return iD;
+    }
+
+    public void setiD(long iD) {
+        this.iD = iD;
+    }
+
+    public long getContactNumber() {
+        return contactNumber;
+    }
+
+    public void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+    
     
 }
