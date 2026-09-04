a35f2896bd6be0c1f0915efd534dee7344dcc87c
estefaniamarquez <estefaniamarquez@unicesar.edu.co>
2026-09-04 17:05:06 -0500

    feat: Development of class "Seller"

--- src/main/java/model/Seller.java
+++ src/main/java/model/Seller.java
@@ -1,8 +1,40 @@
 package model;
 
+//import java.util.ArrayList;
+
 public class Seller {
     //Clase Seller (Vendedor). Hereda todos los atributos de Person pero agrega las características
     //unicas del Vendedor.
     private long employeeCode;
+    private Shift shift;
+//  private ArrayList<Sale> sales;
+//    Se desbloqueara el comentario en el momento que la clase Sale se desarrolle
+
+    public Seller() {
+    }
+
+    public Seller(long employeeCode, Shift shift) {
+        this.employeeCode = employeeCode;
+        this.shift = shift;
+    }
     
+    
+    public long getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(long employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    public Shift getShift() {
+        return shift;
+    }
+
+    public void setShift(Shift shift) {
+        this.shift = shift;
+    }
+    
+    //Metodos de la clase Seller: addSale, searchSale, removeSale
+    //Se agregarán a la clase en el momento en el que la clase Sale esté desarrollada
 }
