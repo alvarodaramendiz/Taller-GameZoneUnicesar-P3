ddf0398797134f12fa23c9bef035faa7e3572670
jahdieldtorres <jahdieldtorres@unicesar.edu.co>
2026-09-05 19:15:53 -0500

    fix: correct stock getter return type in Product

--- src/main/java/model/Product.java
+++ src/main/java/model/Product.java
@@ -1,13 +1,59 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
 /**
  *
  * @author jahdiel
  */
-public class Product {
+
+public abstract class Product {
+    private String productId;
+    private String title;
+    private double price;
+    private int stock;
+
+    public Product(String productId, String title, double price, int stock) {
+        this.productId = productId;
+        this.title = title;
+        this.price = price;
+        this.stock = stock;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+  
+    
+public abstract String getDescription();
+    
     
 }
+    
