ded640f9b8eee17197b13eeb252f64c46c27a576
alvarodaramendiz <alvarodaramendiz@unicesar.edu.co>
2026-09-06 22:39:34 -0500

    feat: Implemented salePersistence class for persistence package com.gamezone.persistence, including prefatorial class imports

---
+++ src/main/java/com/gamezone/persistence/SalePersistence.java
@@ -0,0 +1,27 @@
+package com.gamezone.persistence;
+
+// Importación de clases preliminar
+import com.gamezone.model.Customer;
+import com.gamezone.model.Product;
+import com.gamezone.model.Sale;
+import com.gamezone.model.Seller;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *  Persistencia de ventas que guarda las transacciones realizadas todo el tiempo
+ * 
+ * @author ALVARO
+ */
+public class SalePersistence {
+    // Desarrollo de la persistencia de las transacciones de venta correspondientes al sistema de GameZone
+}
