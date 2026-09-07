9b132c1c9df337168f2ae342b77e0096adb013a1
alvarodaramendiz <alvarodaramendiz@unicesar.edu.co>
2026-09-06 22:40:49 -0500

    feat: Implemented saleService class for logic package com.gamezone.service, only including initial class imports

---
+++ src/main/java/com/gamezone/service/SaleService.java
@@ -0,0 +1,28 @@
+package com.gamezone.service;
+
+// Importación de clases preliminar 
+
+import com.gamezone.model.Customer;
+import com.gamezone.model.Product;
+import com.gamezone.model.Sale;
+import com.gamezone.model.Seller;
+import com.gamezone.persistence.SalePersistence;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Lógica del módulo de ventas que regula transacciones
+ * 
+ * @author ALVARO
+ */
+
+public class SaleService {
+    
+    // Desarrollo de lógica de ventas
+    
+}
