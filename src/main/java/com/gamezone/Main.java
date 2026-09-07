7c7b8300fe513ee9aabacc54de8c003456d47809
alvarodaramendiz <alvarodaramendiz@unicesar.edu.co>
2026-09-06 19:23:20 -0500

    feat: Implemented Main class for parent package com.gamezone

---
+++ src/main/java/com/gamezone/Main.java
@@ -0,0 +1,9 @@
+package com.gamezone;
+
+/**
+ *  Clase principal, conexión entre todas las capas de la arquitectura
+ * @author ALVARO
+ */
+public class Main {
+    // Desarrollo de la clase principal mediante conexiones entre todas las capas, debe utilizarse UI
+}
