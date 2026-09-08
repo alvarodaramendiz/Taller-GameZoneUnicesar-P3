d589444b145e64fd70c53b3ddf84da4ae1e98024
estefaniamarquez <estefaniamarquez@unicesar.edu.co>
2026-09-07 21:49:47 -0500

    feat: Add persistence file paths

---
+++ src/main/java/utilities/PlainArchives.java
@@ -0,0 +1,19 @@
+package utilities;
+
+/**
+ * Utility class that contains the paths to the text files used for the persistence
+ * of the Sale, Purchase, Seller, Customer, Videogame, and Console classes.
+ * If the location or name of a file changes, it only needs to be updated here.
+ * 
+ * @author EstefaniaMarquez
+ */
+
+public class PlainArchives {
+    public static final String PURCHASES = "data/purchases.txt";
+    public static final String SALES   = "data/sales.txt";
+    public static final String SELLERS = "data/sellers.txt";
+    public static final String CUSTOMERS = "data/customers.txt";
+    public static final String VIDEOGAMES = "data/videogames.txt";
+    public static final String CONSOLES = "data/consoles.txt";
+    
+}
