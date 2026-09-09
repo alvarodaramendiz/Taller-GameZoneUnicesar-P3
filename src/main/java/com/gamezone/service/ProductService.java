25ed8ae5762ad44e930b1e98f11f01a3cadc0947
jahdieldtorres <jahdieldtorres@unicesar.edu.co>
2026-09-09 00:15:10 -0500

    feat: implement ProductService with registration n stock validation rules

---
+++ src/main/java/com/gamezone/service/ProductService.java
@@ -0,0 +1,19 @@
+Template inclusion failed (for parameter value "Templates/Licenses/license-default.txt"):
+Template not found for name "Templates/Classes/Templates/Licenses/license-default.txt".
+The name was interpreted by this TemplateLoader: org.netbeans.libs.freemarker.RsrcLoader@
+
+7ddf22d7.
+
+----
+FTL stack trace ("~" means nesting-related):
+	- Failed at: #include "${project.licensePath}"  [in template "Templates/Classes/Class.java" at line 4, column 1]
+----
+package com.gamezone.service;
+
+/**
+ *
+ * @author jahdiel
+ */
+public class ProductService {
+    
+}
